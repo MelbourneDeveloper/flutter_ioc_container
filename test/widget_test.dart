c88a16dc83b78500ab593ba8c617cb50136d0829
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-02-02 20:19:20 +1100

    another test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ioc_container/flutter_ioc_container.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ioc_container/ioc_container.dart';
 
 void main() {
-  testWidgets('basic', (tester) async {
+  testWidgets('basic compose', (tester) async {
     const text = 'test';
     final root = CompositionRoot(
       compose: (builder) => builder.add((container) => text),
+      child: const BasicWidget(),
+    );
+    await tester.pumpWidget(root);
+    expect(find.text(text), findsOneWidget);
+  });
+
+  testWidgets('basic container', (tester) async {
+    const text = 'test';
+    final root = CompositionRoot(
+      container:
+          (IocContainerBuilder()..add((container) => text)).toContainer(),
       child: const BasicWidget(),
     );
     await tester.pumpWidget(root);
