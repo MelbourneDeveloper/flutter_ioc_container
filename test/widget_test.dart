87bdc8b2abcdb649ea5850957f297e9673eb14c8
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-07-01 07:54:29 +1000

    test cleanup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,16 @@ void main() {
   ///The dependency in this case is a text string.
   testWidgets('basic compose', (tester) async {
     const text = 'test';
-    final root = CompositionRoot(
-      compose: BuildCompose((builder) => builder.add((container) => text)),
-      child: const BasicWidget(),
+    await tester.pumpWidget(
+      CompositionRoot(
+        compose: BuildCompose(
+          (builder) => builder.add((_) => text),
+        ),
+        child: const BasicWidget(),
+      ),
     );
-    await tester.pumpWidget(root);
     expect(find.text(text), findsOneWidget);
+    expect(find.byType(BasicWidget), findsOneWidget);
   });
 
   ///This test shows how to create an IoC Container and use it as the source
@@ -22,45 +26,47 @@ void main() {
   ///The dependency in this case is a text string.
   testWidgets('basic container', (tester) async {
     const text = 'test';
-    final root = CompositionRoot(
-      compose: ContainerCompose(
-        (IocContainerBuilder()..add((container) => text)).toContainer(),
+    await tester.pumpWidget(
+      CompositionRoot(
+        compose: ContainerCompose(
+          (IocContainerBuilder()..add((_) => text)).toContainer(),
+        ),
+        child: const BasicWidget(),
       ),
-      child: const BasicWidget(),
     );
-    await tester.pumpWidget(root);
     expect(find.text(text), findsOneWidget);
+    expect(find.byType(BasicWidget), findsOneWidget);
   });
 
   ///This test shows how to provide an asynchronous dependency to your widgets.
   ///The dependency in this case is a future that returns a text string.
   testWidgets('basic async', (tester) async {
     const text = 'test';
-    final root = CompositionRoot.configureBuild(
-      const BasicAsyncWidget(),
-      (builder) => builder.addAsync(
-        (container) async =>
-            Future<String>.delayed(const Duration(seconds: 1), () => text),
+    await tester.pumpWidget(
+      CompositionRoot.configureBuild(
+        const BasicAsyncWidget(),
+        (builder) => builder.addAsync(
+          (_) async => Future.delayed(const Duration(seconds: 1), () => text),
+        ),
       ),
     );
 
-    await tester.pumpWidget(root);
-
     //No text yet
     expect(find.text(text), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
     //Wait for 100 milliseconds
     await tester.pump(const Duration(milliseconds: 100));
 
     //Should see spinner
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text(text), findsNothing);
 
     //Wait 2 seconds
     await tester.pump(const Duration(seconds: 2));
 
     //Should see text
     expect(find.text(text), findsOneWidget);
-
     //Should not see spinner
     expect(find.byType(CircularProgressIndicator), findsNothing);
   });
@@ -70,30 +76,24 @@ void main() {
   ///one instance of class C, which takes two instances of class B.
   ///Each type only ever has one instance in a scope
   testWidgets('basic scoping', (tester) async {
-    final root = CompositionRoot(
-      compose: ContainerCompose(
-        (IocContainerBuilder()
-              ..add(
-                (container) => A(),
-              )
-              ..add(
-                (container) => B(),
-              )
-              ..add(
-                (container) => C(
-                  container<B>(),
-                  container<B>(),
-                ),
-              ))
-            .toContainer(),
+    await tester.pumpWidget(
+      CompositionRoot(
+        compose: ContainerCompose(
+          (IocContainerBuilder()
+                ..add((_) => A())
+                ..add((_) => B())
+                ..add((container) => C(container<B>(), container<B>())))
+              .toContainer(),
+        ),
+        child: const BasicWidgetWithScope(),
       ),
-      child: const BasicWidgetWithScope(),
     );
-    await tester.pumpWidget(root);
     final state = tester
         .state<_BasicWidgetWithScopeState>(find.byType(BasicWidgetWithScope));
     expect(identical(state.one, state.two), isTrue);
     expect(identical(state.c.b1, state.c.b2), isTrue);
+    expect(find.byType(BasicWidgetWithScope), findsOneWidget);
+    expect(find.text('Hi'), findsOneWidget);
   });
 }
 
@@ -102,19 +102,13 @@ class A {}
 class B {}
 
 class C {
-  C(
-    this.b1,
-    this.b2,
-  );
-
+  const C(this.b1, this.b2);
   final B b1;
   final B b2;
 }
 
 class BasicWidget extends StatelessWidget {
-  const BasicWidget({
-    super.key,
-  });
+  const BasicWidget({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -123,16 +117,15 @@ class BasicWidget extends StatelessWidget {
 }
 
 class BasicWidgetWithScope extends StatefulWidget {
-  const BasicWidgetWithScope({
-    super.key,
-  });
+  const BasicWidgetWithScope({super.key});
 
   @override
   State<BasicWidgetWithScope> createState() => _BasicWidgetWithScopeState();
 }
 
 class _BasicWidgetWithScopeState extends State<BasicWidgetWithScope> {
-  late final A one, two;
+  late final A one;
+  late final A two;
   late final C c;
 
   @override
@@ -145,38 +138,26 @@ class _BasicWidgetWithScopeState extends State<BasicWidgetWithScope> {
   }
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: Text('Hi'),
-      );
+  Widget build(BuildContext context) => const MaterialApp(home: Text('Hi'));
 }
 
 class BasicAsyncWidget extends StatefulWidget {
-  const BasicAsyncWidget({
-    super.key,
-  });
+  const BasicAsyncWidget({super.key});
 
   @override
   State<BasicAsyncWidget> createState() => _BasicAsyncWidgetState();
 }
 
 class _BasicAsyncWidgetState extends State<BasicAsyncWidget> {
-  late final Future<String> future;
-
-  @override
-  void didChangeDependencies() {
-    // ignore: discarded_futures
-    future = context.getAsync<String>();
-    super.didChangeDependencies();
-  }
-
   @override
   Widget build(BuildContext context) => MaterialApp(
         home: FutureBuilder(
           // ignore: discarded_futures
-          future: future,
-          builder: (ctx, ss) => ss.connectionState == ConnectionState.done
-              ? Text(ss.data!)
-              : const CircularProgressIndicator(),
+          future: context.getAsync<String>(),
+          builder: (ctx, ss) => switch (ss.connectionState) {
+            ConnectionState.done => Text(ss.data!),
+            _ => const CircularProgressIndicator()
+          },
         ),
       );
 }
